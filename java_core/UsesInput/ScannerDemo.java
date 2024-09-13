e974c6f36d2183929c1470b9c96aaf8b7de4dddd
trunghieu1109 <101007868+trunghieu1109@users.noreply.github.com>
2024-09-13 18:41:19 +0700

    delete due to some errors

--- java_core/UsesInput/ScannerDemo.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ScannerDemo {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        float num1 = scanner.nextFloat();
-        System.out.print(num1);
-    }
-}
