965d9b3ca9b32af470ad8823c7b6561d25de9feb
trunghieu1109 <101007868+trunghieu1109@users.noreply.github.com>
2024-09-17 10:33:05 +0700

    add get user page and other crud query with spring data jpa

--- java_spring_core/DemoWeb/demo/src/main/java/com/example/demo/model/User.java
+++ java_spring_core/DemoWeb/demo/src/main/java/com/example/demo/model/User.java
@@ -1,48 +1,4 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity(name = "tbl_user")
-public class User extends AbstractEntity {
-
-    @Column(name="first_name")
-    private String firstName;
-
-    @Column(name="last_name")
-    private String lastName;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name="date_of_birth")
-    private Date dateOfBirth;
-
-    @Column(name="username")
-    private String userName;
-
-    @Column(name="password")
-    private String passWord;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
-    private Set<Address> addresses = new HashSet<>();
-
-    public void saveAddress(Address address) {
-        if (addresses == null) {
-            addresses = new HashSet<>();
-        }
-
-        addresses.add(address);
-        address.setUser(this);
-    }
-
+public class User {
 }
